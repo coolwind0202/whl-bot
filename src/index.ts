efd7947193d95c1efe5ad20d3bb2ce7c2b6ba96c
coolwind0202 <tororo_shidoro@yahoo.co.jp>
2021-10-01 17:33:53 +0900

    Change index.ts Use DISCORD_TOKEN environment variable directly

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { Client, Intents } from "discord.js";
 import dotenv from "dotenv";
-import fs from "fs";
 
 dotenv.config();
 
@@ -13,31 +12,5 @@ client.once('ready', () => {
 });
 console.log(process.env.DISCORD_TOKEN);
 
-const getToken = async () => {
-	const envToken = process.env.DISCORD_TOKEN;
-	try {
-		const file = await fs.promises.readFile("/run/secrets/discord_token", "utf-8");
-		
-		if (file) {
-			return file;
-		} else if (envToken) {
-			return envToken;
-		} else return null;
-
-	} catch (e) {
-		console.log(e);
-		return envToken || null;
-	}
-}
-
 // Login to Discord with your client's token
-
-const token = getToken();
-token
-.then(value => {
-	if (value) client.login(value)
-	else console.error("Bot のトークンが見つかりませんでした。\n" +
-		"トークンの設定方法は、 https://github.com/white-lucida/discord_bot/blob/main/README.md を参照してください。"
-	);
-})
-.catch(() => console.error());
+client.login(process.env.DISCORD_TOKEN);
